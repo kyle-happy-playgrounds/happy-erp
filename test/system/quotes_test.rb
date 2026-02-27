0abbefa04a8ef27fc47925b46bcb8ec38a79d171
Kyle Brown <253535757+kyle-happy-playgrounds@users.noreply.github.com>
2026-02-27 08:45:53 -0600

    Check address_type isn't nil + added new system test for quotes.

--- test/system/quotes_test.rb
+++ test/system/quotes_test.rb
@@ -31,4 +31,45 @@ class QuotesTest < ApplicationSystemTestCase
       assert_text "#{quote.number}-#{quote.sub}"
     end
   end
+
+  test "should add and remove new line item for quote" do
+    quote = HappyQuote.first
+    visit edit_happy_quote_path(quote)
+    assert_field "Tax Rate %", with: "0.1"
+    assert_field "Tax Total", with: "14.10"
+    assert_field "Taxable Amount", with: "14096.00"
+
+    #add new line
+    click_link "add line"
+    within all('.nested-fields').last do
+      fill_in 'product_id', with: '0713488'
+      fill_in "Quantity", with: "1"
+      assert_field "Margin %", with: "35.00"
+      assert_field "Quote subtotal", with: "14,205.52"
+      assert_field "Quote margin", with: "58.87"
+    end
+
+    #check quote numbers with new line added
+    assert_field "Tax Rate %", with: "0.1"
+    assert_field "Tax Total", with: "14.21"
+    assert_field "Taxable Amount", with: "14205.52"
+
+    #remove line
+    within all('.nested-fields').last do
+      click_link "remove line"
+    end
+
+    #check updated subtotal
+    within all('.nested-fields').last do
+      assert_field "Margin %", with: "73.00"
+      assert_field "Quote subtotal", with: "14,096.00"
+      assert_field "Quote margin", with: "59.05"
+    end
+
+    #check quote numbers updated back
+    assert_field "Tax Rate %", with: "0.1"
+    assert_field "Tax Total", with: "14.10"
+    assert_field "Taxable Amount", with: "14096.00"
+  end
+
 end
